f4c45d24a0430b46f1c4aba3fb6281bd2bd708a3
Fr3nk21 <104112709+Fr3nk21@users.noreply.github.com>
2023-10-31 07:09:36 +1100

    Final adjustments

--- 21. Physics/src/script.js
+++ 21. Physics/src/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 
 THREE.ColorManagement.enabled = false
 
@@ -36,8 +36,25 @@ debugObject.createBox = () =>
     )
 }
 
+debugObject.reset = () =>
+{
+    for(const object of objectsToUpdate)
+    {
+        // Remove body
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        // Remove mesh
+        scene.remove(object.mesh)
+    }
+
+    objectsToUpdate.splice(0, objectsToUpdate.length)
+
+}
+
 gui.add(debugObject, 'createSphere')
 gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
 
 /**
  * Base
